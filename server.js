7b598720a00ea585add6f79f45b875d79ca01c18
Cesar-Quintero <cesarqdev@gmail.com>
2022-09-29 12:44:24 -0700

    add social links and remove redundant code

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const flash = require("express-flash");
 const logger = require("morgan");
 const connectDB = require("./config/database");
 const mainRoutes = require("./routes/main");
-const postRoutes = require("./routes/posts");
 const availabilityRoutes = require("./routes/shifts")
 
 //Use .env file in config folder
@@ -56,7 +55,6 @@ app.use(flash());
 
 //Setup Routes For Which The Server Is Listening
 app.use("/", mainRoutes);
-app.use("/post", postRoutes);
 app.use("/shift", availabilityRoutes);
 
 //Server Running
